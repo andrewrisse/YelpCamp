2a56882fed4449d1f6a2b4a1c2b1c6d215dc8d33
Andrew Risse <risse@usc.edu>
2019-11-11 23:57:22 +0000

    remove || 3000 from app.listen

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use("/",indexRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 app.use("/campgrounds",campgroundRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("The YelpCamp Server is running!");
 });
 
